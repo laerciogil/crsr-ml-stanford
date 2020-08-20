8710bed20fb409d362fb4c9e2623c00d77e217cf
laerciogil <laerciogilpb@gmail.com>
2020-08-20 03:40:53 -0300

    Mandatory exercises

--- ml-01-linear-regression/ex1/gradientDescent.m
+++ ml-01-linear-regression/ex1/gradientDescent.m
@@ -16,12 +16,13 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
+    
+    
+    
+    theta0 = theta(1,1) - (alpha/m) * sum(((X * theta) - y) .* X(:,1));
+    theta1 = theta(2,1) - (alpha/m) * sum(((X * theta) - y) .* X(:,2));
+    
+    theta = [theta0; theta1];
 
     % ============================================================
 
